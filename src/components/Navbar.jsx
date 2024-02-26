d2d5de19a1e7a517a263278827cca71e26cf7760
oladayo Adekoya <adekoyadayo2010@gmail.com>
2024-02-26 09:50:55 +0100

    fix search functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useGlobalContext } from "./Context";
 import logo from "../images/newlogo.png";
+import axios from "axios";
 
 const zones = ["d", "e", "h", "i", "j", "k", "l", "m", "n", "q", "r"];
 
@@ -54,11 +55,20 @@ const Navbar = () => {
       try {
         const fetchedData = [];
 
+        // fetch data
+        const response = await axios.get(baseURL);
+        const response_data = response.data;
+
         for (let i = 0; i < zones.length; i++) {
-          const response = await fetch(`${baseURL}/zone_${zones[i]}s`);
-          const zone_data = await response.json();
+          const zone_data = response_data.filter(
+            (asset) =>
+              asset.custom_fields["Building Zone"]?.value
+                .slice(-1)
+                .toLowerCase() === zones[i]
+          );
           fetchedData.push(...zone_data);
         }
+        console.log(fetchedData);
         setData((prevData) => [...prevData, ...fetchedData]);
       } catch (error) {
         console.log(error);
@@ -72,9 +82,9 @@ const Navbar = () => {
   if (data) {
     data.map((desk) => {
       searchData.push({
-        key: `${desk.desk_id.toLowerCase()}`,
-        value: desk.desk_id,
-        id: desk.id,
+        key: `${desk.custom_fields["Workspace"]?.value.toLowerCase()}`,
+        value: desk.custom_fields["Workspace"]?.value.toLowerCase(),
+        id: desk?.id,
       });
     });
   }
@@ -89,12 +99,7 @@ const Navbar = () => {
         <div className="relative flex h-16 items-center justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
-              <img
-                className="h-8 w-auto"
-                // src="https://www.outsourceglobal.com/logo.png"
-                src={logo}
-                alt="Your Company"
-              />
+              <img className="h-8 w-auto" src={logo} alt="Your Company" />
             </div>
 
             <div className="ml-24">
@@ -151,7 +156,7 @@ const Navbar = () => {
                     value={query}
                     onChange={handleSearchChange}
                     placeholder="Search Desk_ID"
-                    className="bg-gray-50 border outline-sky-300 border-gray-300 focus:ring-blue-500 focus:border-blue-500 text-gray-900 text-sm rounded-full block w-full py-1 px-2.5"
+                    className="bg-gray-50 border outline-sky-300 border-gray-300 focus:ring-blue-500 focus:border-blue-500 text-gray-900 text-sm rounded rounded-r-full block w-full py-1 px-2.5"
                   />
                   <button
                     type="button"
@@ -159,10 +164,10 @@ const Navbar = () => {
                       setQuery("");
                       setSearchedDesk("");
                     }}
-                    className="absolute inset-y-0 right-0 flex items-center pr-1 bg-sky-200 hover:bg-sky-300 rounded-r-full"
+                    className=" absolute inset-y-0 right-0 flex items-center pr-1 bg-sky-200 hover:bg-sky-300 rounded-r-full"
                   >
                     <svg
-                      className="w-7 h-7 text-gray-500 hover:text-gray-900"
+                      className="w-7 h-7 text-gray-500 hover:text-gray-900 pl-1"
                       viewBox="0 0 24 24"
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
